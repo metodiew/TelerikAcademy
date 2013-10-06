05ef4dc2eabec8e04f49c0ca8812bc5ef9efac1b
metodiew <stanko@metodiew.com>
2013-10-06 14:08:05 +0300

    Re upload of header.php

---
+++ PHP/messages/inc/header.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+
+error_reporting( E_ALL );
+ini_set( 'display_errors', 1 );
+
+require_once 'inc/functions.php';
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title><?php echo $pageTitle; ?></title>
+	<link rel="stylesheet" type="text/css" href="style/style.css">
+</head>
+<body>
+	<div id="wrapper">
+		<?php require_once 'inc/menu.php'; ?>
+		<div id="content">
